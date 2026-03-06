1c924871700be6e548ad3ad6a4049f70554b4477
Gesser Beatriz (SO/ETS-BR) <geb1jvl@bosch.com>
2026-03-06 11:13:48 -0300

    first commit

---
+++ src/app/services/wather.service/weather.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { WeatherService } from '../wather.service/weather.service';
+
+describe('WeatherService', () => {
+  let service: WeatherService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(WeatherService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});

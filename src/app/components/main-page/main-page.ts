48dcc4880067fd758fb386a2bcfa5bc60fd27afc
Gesser Beatriz (SO/ETS-BR) <geb1jvl@bosch.com>
2026-03-06 11:38:39 -0300

    input

--- src/app/components/main-page/main-page.ts
+++ src/app/components/main-page/main-page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { CepInput } from '../cep-input/cep-input';
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [CepInput],
   templateUrl: './main-page.html',
   styleUrl: './main-page.css',
 })

ca7f445cdfe23362059feede2e3581ec5db1c5c6
Gesser Beatriz (SO/ETS-BR) <geb1jvl@bosch.com>
2026-03-10 08:20:35 -0300

    ngmodel

--- src/app/components/cep-input/cep-input.ts
+++ src/app/components/cep-input/cep-input.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cep-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './cep-input.html',
   styleUrl: './cep-input.css',
 })
 export class CepInput {
 
+cep: string = '';
+
+getCep() {
+  console.log(this.cep);
+}
 }
